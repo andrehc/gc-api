ebebe8dc50b064273973519bf483d357da248568
andre_hilario <andre.costa@bernoulli.com.br>
2024-02-16 15:49:56 -0300

    create test

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_can_create_user() {
+        $user = \App\Models\User::factory()->make();
+        $this->assertInstanceOf(\App\Models\User::class, $user);
+    }
+}
